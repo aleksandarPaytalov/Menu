2d2400f8d5be79787020a3ce048beab7b2ff61d1
aleksandarPaytalov <apaytalov89@gmail.com>
2026-01-19 17:33:44 +0200

    implement a filter functionality base on a category of the item

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,20 @@ const uniqueCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(uniqueCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const filteredItems = menu.filter((item) => item.category === category);
+    setMenuItems(filteredItems);
+  };
 
   return (
     <main>
       <section className="menu">
         <Title text="Our menu" />
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
